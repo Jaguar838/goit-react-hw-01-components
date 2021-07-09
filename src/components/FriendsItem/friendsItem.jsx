2c743d1aab44bb4250dcaf814bf9823cf93c6391
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-09 14:28:40 +0300

    Update friendsItem.jsx

--- src/components/FriendsItem/friendsItem.jsx
+++ src/components/FriendsItem/friendsItem.jsx
@@ -1,3 +1,9 @@
 import React from 'react'
 import styles from './friendsItem.scss';
 import PropTypes from 'prop-types'
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="" width="48" />
+  <p class="name"></p>
+</li>
