c5379b05e319a6f6e51034bf7d593916ca653a4a
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-09 10:45:51 +0300

    add import components App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Container} from './components/Container/container'
+import userData from './server/user.json';
+import Profile from './components/Profile/profile';
+import avatarNone from './server/avatarNone.jpg';
+import friendsData from './server/friends.json';
+import FriendsItem from './components/FriendsItem/friendsItem';
+import statsData from './server/stats.json';
+import Statistics from './components/Statistics/statistics'
+import transactionsData from './server/transactions.json';
+import Transactions from './components/Transactions/transactions';
+
 
 const App = () => {
     return (
         <>
-            <h2>heloo</h2>
+            <Container>Hello World</Container>
         </>
     );
 }
--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import avatarNone from "./avatarNone.jpg";
-import styles from "./profile.scss";
+
+import styles from './profile.scss';
