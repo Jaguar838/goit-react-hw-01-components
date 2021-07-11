23bfd91ad52fa3113e13ae4b1a2c8bad88b05b22
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-11 20:26:46 +0300

    Up FI

--- src/components/FriendList/FriendItem/friendItem.jsx
+++ src/components/FriendList/FriendItem/friendItem.jsx
@@ -11,3 +11,17 @@ const friends = {
 
 
 
+export const FriendItem = ({ avatar, name, isOnline }) => {
+  const styleIsOnline = isOnline
+    ? 'online statusFriends'
+    : 'offline statusFriends';
+
+  return (
+    <li className="itemFriend">
+      <span className={styleIsOnline}></span>
+      <img className="avatarFriends" src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+};
+
