3ea252b249452d37b9bcfd6ae5c957732a8cbfbb
Jaguar838 <oleg.yamgurov@gmail.com>
2021-07-11 15:25:30 +0300

    Update friendItem.jsx

--- src/components/FriendList/FriendItem/friendItem.jsx
+++ src/components/FriendList/FriendItem/friendItem.jsx
@@ -1,8 +1,13 @@
 import styles from './friendItem.scss';
 import PropTypes from 'prop-types'
 
+const friends = {
 <li class="item">
   <span class="status"></span>
   <img class="avatar" src="" alt="" width="48" />
   <p class="name"></p>
 </li>
+}
+
+
+
